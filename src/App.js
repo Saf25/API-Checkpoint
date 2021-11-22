347848a0e12cade1c56343c4031d71831107a315
Saf25 <90209792+Saf25@users.noreply.github.com>
2021-11-22 20:26:25 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import DataFetching from "./DataFetching";
+import "./App.css";
+const App = () => {
+  return (
+    <div className="App-header">
+      <DataFetching></DataFetching>
+    </div>
+  );
+};
+export default App;
---
+++ src/DataFetching.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+const DataFetching = () => {
+  const [listOfUsers, setlistOfUsers] = useState([]);
+  const [id, setid] = useState({});
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        console.log(res);
+        setlistOfUsers(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div className="mainlist">
+      <h2>Below is the list of Users</h2>
+      <ul>
+        {listOfUsers.map((users) => (
+          <li style={{ listStyleType: "none" }} key={users.id}>
+            <span> Name: </span>
+            {users.name}
+            <ul className="list">
+              <span style={{ color: "green", fontStyle: "italic" }}>
+                Username:{"  "}
+              </span>{" "}
+              {users.username}
+              <br></br>
+              <span style={{ color: "blue" }}>E-mail:{"  "} </span>
+              <u>{users.email}</u>
+            </ul>
+          </li>
+        ))}
+      </ul>
+      <br></br>
+    </div>
+  );
+};
+
+export default DataFetching;
